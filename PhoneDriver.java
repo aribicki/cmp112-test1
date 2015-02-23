9e9928a2f432f990946a199c8af6ea1012f4454e
Andrew <aribicki@gmail.com>
2015-02-23 16:52:37 -0500

    stuck on error again

---
+++ PhoneDriver.java
@@ -0,0 +1,24 @@
+// Andrew Ribicki
+// 2/23/2015 
+// CMP 112A Test 
+
+ public class PhoneDriver {
+ 	public static void main(String[] args) {
+
+	Phone phone1 = new Phone();
+	phone.name = "cell";
+	phone.make = "samsung";
+	System.out.println(phone1.toString());
+
+	Phone phone2  = new Phone();
+	phone.name = "cell";
+	phone.make = "LG";
+	System.out.println(phone2.toString());
+
+	Phone phone3 = new Phone();
+	phone.name = "cell";
+	Phone.make = "Motorola"; 
+	System.out.println(phone3.toString());
+}
+
+}
